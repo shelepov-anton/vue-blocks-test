cde80c2e1b1b25d7d743d45201bab5a1d2c50a3e
Anton Shelepov <ashelepov@baccasoft.ru>
2025-04-17 03:06:22 +1000

    update

---
+++ src/components/BlockSelectedMultiply.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import Item from '@/components/Item.vue'
+import type { BlockItem } from '@/utils/types.ts'
+
+defineProps<{
+  selected: BlockItem[]
+}>()
+
+const emit = defineEmits<{
+  (e: 'unselect', id: number): void
+}>()
+
+function onUnselect(id: number) {
+  emit('unselect', id)
+}
+
+</script>
+
+<template>
+  <div class="container-bordered">
+    <Item v-for="item of selected" :name="item.name" @click="onUnselect(item.id)" />
+  </div>
+</template>
