748a8436322497d6f61013ff600112268a4295e8
Anton Shelepov <ashelepov@baccasoft.ru>
2025-04-17 02:58:05 +1000

    update

---
+++ src/components/BlockBottom.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import type { BlockItem } from '@/utils/types.ts'
+import Item from '@/components/Item.vue'
+
+defineProps<{
+  items: BlockItem[]
+}>()
+
+const emit = defineEmits<{
+  (e: 'select', item: BlockItem): void
+}>()
+
+function onSelect(item: BlockItem) {
+  emit('select', item)
+}
+
+</script>
+
+<template>
+  <div class="container-bordered">
+    <Item v-for="item of items" :name="item.name" @click="onSelect(item)" />
+  </div>
+</template>
---
+++ src/components/Item.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import type { BlockItem } from '@/utils/types.ts'
+
+defineProps<{
+  name: string
+}>()
+</script>
+
+<template>
+  <div class="item-container">
+    {{ name }}
+  </div>
+</template>
+<style lang="css" scoped>
+.item-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  white-space: nowrap;
+  font-size: 18px;
+
+  cursor: pointer;
+  user-select: none;
+
+  width: 100px;
+  height: 70px;
+
+  border: black 2px solid;
+}
+</style>
